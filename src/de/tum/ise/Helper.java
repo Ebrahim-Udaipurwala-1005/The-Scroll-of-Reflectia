c21435d9d21911844f4e42c6902e96e84705323e
Andrei Koshelev <andrei.koshelev@tum.de>
2025-04-30 13:38:47 +0200

    Exercise-Template pushed by Artemis

---
+++ src/de/tum/ise/Helper.java
@@ -0,0 +1,8 @@
+package de.tum.ise;
+
+public class Helper {
+    public static String findLongestChant(String s){
+        /// TODO: Write your code here
+        return null;
+    }
+}
---
+++ src/de/tum/ise/Main.java
@@ -0,0 +1,8 @@
+package de.tum.ise;
+
+public class Main {
+    public static void main(String[] args) {
+        /// TODO: Test your code here. However, this is not part of the grade.
+        System.out.println("Hello World!");
+    }
+}
